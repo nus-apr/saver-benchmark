cfefdbf55d393b5948ac588787b2a634a6bb0d93
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-03-17 18:46:08 +0800

    update setup scripts for swool

--- swoole/memory-leak-10/setup.sh
+++ swoole/memory-leak-10/setup.sh
@@ -13,5 +13,5 @@ download_link=https://raw.githubusercontent.com/nus-apr/saver-benchmark/main/arc
 mkdir tmp
 wget $download_link
 tar -xzf $project_name.tar.gz -C tmp
-mv tmp/$project_name src
+mv tmp/$project_name-src src
 rm -rf tmp
